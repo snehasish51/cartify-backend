5e79821fbcd7c45be758616b0fed2c9f8e8a9798
snehasish51 <snehasishg029@gmail.com>
2025-09-03 18:58:57 +0530

    Add user GET POST and PATCH endpoints for REST API

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
 
-// Load Firebase key from environment variable
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
@@ -15,12 +14,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Test endpoint
+//test
 app.get("/", (req, res) => {
   res.send("Hello from Render + Firestore API!");
 });
 
-// Example endpoint: Get all categories
+//Get all categories
 app.get("/categories", async (req, res) => {
   try {
     const snapshot = await db.collection("categories").get();
@@ -34,6 +33,44 @@ app.get("/categories", async (req, res) => {
     res.status(500).send("Error fetching categories");
   }
 });
+
+//post users
+app.post("/users", async (req, res) => {
+  try {
+    const { email, password, firstName, lastName } = req.body;
+    if (!email || !password || !firstName || !lastName) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName: `${firstName} ${lastName}`,
+    });
+    const uid = userRecord.uid;
+    await db.collection('users').doc(uid).set({
+      firstName,
+      lastName,
+      email,
+      phone: "",
+      cart: [],
+      wishlist: [],
+      addresses: [],
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    const userRef = db.collection('users').doc(uid);
+    const userDoc = await userRef.get();
+
+    res.status(201).json({
+      message: "User created",
+      uid,
+      user: { id: uid, ...userDoc.data() }
+    });
+  }
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
